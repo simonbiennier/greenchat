5b10dabf5e7233d3de2ea2fbc72860c994b8cbe1
Simon <simon@biennier.fr>
2025-03-26 22:08:42 +0100

    fix logo props

--- packages/website/src/components/Logo.tsx
+++ packages/website/src/components/Logo.tsx
@@ -1,10 +1,10 @@
-import type { DetailedHTMLProps, HTMLAttributes } from "react"
+import type { ComponentPropsWithoutRef } from "react"
 
 import logo from "@/images/logo.png"
 import { clsx } from "clsx"
 import Image from "next/image"
 
-export function Logo(props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
+export function Logo(props: ComponentPropsWithoutRef<"div">) {
   return (
     <div {...props} className={clsx("flex gap-2 justify-center items-center", props.className)}>
       <Image alt="GreenChat" className="h-10 w-auto" src={logo} />
